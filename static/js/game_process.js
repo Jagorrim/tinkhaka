225e24f3d5e8fe746ab37c30a72149d0fe38264e
jagorrim <jagorinm@Mail.ru>
2024-06-09 18:18:38 +0500

    Пупупупупу

--- static/js/game_process.js
+++ static/js/game_process.js
@@ -1,24 +1,29 @@
 var count = 0;
 var data = new Array();
 
-$.ajax({
-    url: "/",
-    type: "POST",
-    contentType: 'application/json',
-    data: JSON.stringify({}),
-    success: function (result) {
-        count = result[0].length;
-        let main_part = $('#main');
-        for (let i = 0; i < count; i++) {
-            main_part.html(
-                main_part.html() + `<p id=${i}>${result[0][i]}</p><input id=${i} placeholder="Ваш ответ">`
-            )
-        }
-        data = result;
+function update() {
+    $.ajax({
+        url: "/",
+        type: "POST",
+        contentType: 'application/json',
+        data: JSON.stringify({}),
+        success: function (result) {
+            count = result[0].length;
+            let main_part = $('#main');
+            main_part.html('');
+            for (let i = 0; i < count; i++) {
+                main_part.html(
+                    main_part.html() + `<p id=${i}>${result[0][i]}</p><input id=${i} placeholder="Ваш ответ">`
+                )
+            }
+            data = result;
 
-    },
-    error: function () { return 'error' },
-})
+        },
+        error: function () { return 'error' },
+    })
+}
+
+update();
 
 $('#check').on("click", function () {
     let inputs = document.querySelectorAll("input");
